b5f4b12fa96351fdd6702e5d8993c5a03bda4409
kakunpc <15257475+kakunpc@users.noreply.github.com>
2018-08-02 20:22:25 +0900

    add project

---
+++ Packages/Assets2Packages/Editor/Assets2Packages.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Packages.Assets2Packages.Editor
+{
+#if UNITY_2018_2_OR_NEWER
+
+    public class Assets2Packages : EditorWindow
+    {
+        const string SamplePackage = "com.unity.sample";
+
+        string category = "Unity/AssetStore";
+        string description = "";
+        string displayName = "";
+        Object folder;
+        string keywords = "";
+        string packageName = SamplePackage;
+        string unity = "2018.2";
+        string version = "1.0.0";
+
+        [MenuItem("Window/Assets 2 Packages")]
+        static void Init()
+        {
+            var window = (Assets2Packages)GetWindow(typeof(Assets2Packages));
+            window.Show();
+            window.titleContent = new GUIContent("Assets 2 Packages");
+        }
+
+        bool CheckSettings()
+        {
+            return folder != null &&
+                   string.IsNullOrEmpty(packageName) == false &&
+                   packageName != SamplePackage;
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+
+            var newFolder = EditorGUILayout.ObjectField("Folder", folder, typeof(Object), false);
+            var path = AssetDatabase.GetAssetPath(newFolder);
+            if (path.StartsWith("Packages/")) path = string.Empty;
+            folder = AssetDatabase.IsValidFolder(path) ? newFolder : null;
+            packageName = EditorGUILayout.TextField("Package Name", packageName);
+            displayName = EditorGUILayout.TextField("Display Name", displayName);
+            category = EditorGUILayout.TextField("Category", category);
+            description = EditorGUILayout.TextField("Description", description);
+            unity = EditorGUILayout.TextField("Unity", unity);
+            version = EditorGUILayout.TextField("Version", version);
+            keywords = EditorGUILayout.TextField("Keywords", keywords);
+
+            EditorGUI.BeginDisabledGroup(CheckSettings() == false);
+            if (GUILayout.Button("Move Start")) Move();
+            EditorGUI.EndDisabledGroup();
+        }
+
+        void Move()
+        {
+            var unityPackageInfo = new UnityPackageInfo
+            {
+                category = category,
+                description = description,
+                keywords = keywords.Split(','),
+                name = packageName.ToLower(),
+                displayName = displayName,
+                unity = unity,
+                version = version
+            };
+            var json = JsonUtility.ToJson(unityPackageInfo);
+
+            var packageDirectory = Path.Combine(Application.dataPath, $"../Packages/{packageName}");
+
+            var path = Path.Combine(Application.dataPath,
+                AssetDatabase.GetAssetPath(folder).Substring("Assets/".Length));
+
+            Directory.Move(path, packageDirectory);
+
+            using (var writer = new StreamWriter(packageDirectory + "/package.json", false))
+            {
+                writer.WriteLine(json);
+            }
+            AssetDatabase.Refresh();
+            Close();
+        }
+
+        [Serializable]
+        public class UnityPackageInfo
+        {
+            public string category;
+            public string description;
+            public string displayName;
+            public string[] keywords;
+            public string name;
+            public string unity;
+            public string version;
+        }
+    }
+
+#endif
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Assets2Packages
+
+![Image1](https://raw.githubusercontent.com/kakunpc/Assets2Packages/master/img/Assets2Packages.gif "Assets2Packages")
+
+Assets にある物を、ディレクトリごとPackagesに移動させる事ができるEditor拡張
+
+# 使い方
+1. Githubからプロジェクトをクローンするかzipでダウンロードします。
+1. このプロジェクトの中にある `Packages/Assets2Packages` ディレクトリを `YoureProjectPath/Packages` へコピーします
+1. Unityを起動します。
+1. メニューの Window -> Assets2Packages を選択します。
+1. 設定項目を入力して、 `Move Start` をクリック
+
+# 動作環境
+- Unity2018.2
+- .NET 4.x Equivalent
+
+# LICENSE
+MIT License Copyright (c) 2018 kakunpc Released under the MIT license
+
+https://github.com/kakunpc/Assets2Packages/blob/master/LICENSE
